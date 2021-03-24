8c7596a94663f12fcb70bfde2b2429a9f4e2b882
orlovstanislav1 <orlovstanislav1@rambler.ru>
2021-03-24 22:27:24 +0300

    JavaRush

---
+++ Java-Rush/JR5/JR1_27/Solution.java
@@ -0,0 +1,28 @@
+package JR5.JR1_27;
+
+/*
+Вывести на экран сегодняшнюю дату
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+        TimeDate timeDate = new TimeDate(21, 02, 2014);
+        System.out.println(timeDate);
+    }
+
+    public static class TimeDate {
+        int min;
+        int month;
+        int year;
+
+        public TimeDate(int min, int month, int year) {
+            this.min = min;
+            this.month = month;
+            this.year = year;
+        }
+
+        public String toString() {
+            return (min + " " + month + " " + year);
+        }
+    }
+}
