faef8de780f0224a77deb58658ac70bdb0203bb7
orlovstanislav1 <orlovstanislav1@rambler.ru>
2021-03-25 21:53:59 +0300

    JavaRush

---
+++ Java-Rush/JR5/JR1_27/Solution1.java
@@ -0,0 +1,15 @@
+package JR5.JR1_27;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+Вывести на экран сегодняшнюю дату
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        Date dateNow = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy MM dd");
+        System.out.println(simpleDateFormat.format(dateNow));
+    }
+}
